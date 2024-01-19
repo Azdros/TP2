c7f4e236e16089ab9affb0e4cf47eac45432b897
Azdros <adrosflottes@aol.com>
2024-01-19 12:07:44 +0100

    Triangle fini

--- exercice1.c
+++ exercice1.c
@@ -10,6 +10,24 @@ int pascal(int i,int j)
         }
     }
     else{
-        return(pascal(i-1,j)+pascal(i-1,j-1));
+        return(pascal(i-1,j-1)+pascal(i-1,j));
     }
 }
+void main(){
+    int N=0;
+    int i=0;
+    int j=0;
+    printf("Insérez N\n");
+    scanf("%d",&N);
+    while(i<N){
+        while(j<i+1){
+            printf("%i ",pascal(i,j));
+            j++;
+        }
+        printf("\n");
+        j=0;
+        i++;
+    }
+}
+/*Pour N = 10 : durée de 0.276s
+Pour N = 100 : durée trop longue pour la mesurer durant la séance/*
